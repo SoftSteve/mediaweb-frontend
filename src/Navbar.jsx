e80747a0fe17d7fe26edc73927fd059800d2e732
softSteve <stevenhouk14@gmail.com>
2025-07-23 11:00:16 -0400

    styling

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -241,7 +241,7 @@ export default function NavBar() {
                     Are you sure you want to sign out?
                   </p>
                 </div>
-                <div className="mt-4 flex justify-end gap-3">
+                <div className="mt-4 flex justify-center gap-3">
                   <button
                     className="px-4 py-2 bg-surface text-gray-800 rounded-xl hover:bg-gray-200"
                     onClick={() => setShowLogoutConfirm(false)}
